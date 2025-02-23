5777eacf1999c8c3b4aaf2c1735b94f11d714479
etcrypt <ethialcrypt@gmail.com>
2025-02-23 20:51:04 +0530

    Add Data types for the code

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 def main():
     print("Hello World")
     
-def add(a, b):
+def add(a:int, b:int) -> int:
     return a+b
 
-def mul(a, b):
+def mul(a:int, b:int) -> int:
     return a*b
 
-def division(a, b):
+def division(a:int, b:int) -> int:
     return a/b
 
 if __name__ == "__main__":
