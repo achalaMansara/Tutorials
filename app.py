432d09877a820bfe69c45e8099229128e67886ba
etcrypt <ethialcrypt@gmail.com>
2025-02-23 20:41:04 +0530

    Init Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+def main():
+    print("Hello World")
+    
+def add(a, b):
+    return a+b
+
+if __name__ == "__main__":
+    main()
+    result = add(int(input("Enter A Number: ")), int(input("Enter A Number: ")))
+    print(f'The result is: {result}')
